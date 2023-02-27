4b538e5d8bae563db0d02228df26d54505f8f90f
halo3rocksanthony <halo3rocksanthony@gmail.com>
2023-02-26 23:06:47 -0700

    Done

--- project/users/forms.py
+++ project/users/forms.py
@@ -12,19 +12,17 @@ class BaseForm(forms.Form):
 
 # # Create a form class that inherits from BaseForm
 class RegistrationForm(BaseForm):
-    email = forms.EmailField()
-    password_confirmation = forms.CharField(min_length=8, max_length=32)
+    email = forms.EmailField(
+         widget=forms.TextInput(attrs={'placeholder':'Email'})
+    )
+    password_confirmation = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder':'Confirm Password'}), min_length=8, max_length=32)
+    
 
-# class RegistrationModelForm(BaseForm):
-#     class Meta:
-#         model=RegistrationModel
-#         fields=['username', 'password', 'email']
-#         # exlude=['username']
+
 class MonthlyExpenseForm(forms.Form):
     rent_or_mortgage_payment=forms.IntegerField()
     car_payment =forms.IntegerField()
-    # mortgage_bill=forms.IntegerField()
-    # mortgage_interest_rate=forms.IntegerField()
     groceries=forms.IntegerField()
     dining_out=forms.IntegerField()
     gas=forms.IntegerField()
@@ -34,21 +32,9 @@ class MonthlyExpenseForm(forms.Form):
     miscellaneous=forms.IntegerField()
 
 class AssetsForm(forms.Form):
-    # select_choices=(
-    #     ('Yes', 'Yes'),
-    #     ('No', 'No'),
-    # )
-
     monthly_income=forms.IntegerField()
     amount_in_savings=forms.IntegerField()
-    # interest_rate_on_savings_account=forms.IntegerField()
     amount_in_stocks=forms.IntegerField()
-    # investing_in_401K=forms.ChoiceField(choices=select_choices)
-    # interest_401K_match=forms.IntegerField()
-    # monthly_amount_investing_in_roth=forms.IntegerField()
-    # roth_amount=forms.IntegerField()
-    # pass_miscellaneous=forms.IntegerField()
-    # user = forms.ForeignKey(BaseModel, on_delete=models.CASCADE)
 
 class MiscFinancesForm(forms.Form):
     select_choices=(
@@ -66,9 +52,6 @@ class BudgetReviewForm(forms.Form):
     Total_Monthly_Savings=forms.IntegerField()
     Total_Monthly_Expenses=forms.IntegerField()
     Under_Over_Budget=forms.IntegerField()
-    # user = models.ForeignKey(BaseModel, on_delete=models.CASCADE)
-
-# class FinancialInfo(MonthlyExpenseForm, AssetsForm):
 
 
 
